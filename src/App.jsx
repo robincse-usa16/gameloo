8c6adfd21dbb0e68a52ebacba8458ee09810e3e4
robincse-usa16 <robincse.usa16@gmail.com>
2026-02-03 16:47:21 +0600

    all code ar same. onek valo kaj korsi

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+
 import Home from "./pages/Home";
 
-import OfferDetails from "./pages/OfferDetails";
 import ExclusiveOffers from "./pages/ExclusiveOffers";
+import OfferDetails from "./pages/OfferDetails";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="offers" element={<ExclusiveOffers />} />
-        <Route path="/offers/:slug" element={<OfferDetails />} />
+        <Route path="/offers" element={<ExclusiveOffers />} />
+        <Route path="/offer/:slug" element={<OfferDetails />} />
       </Routes>
+
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
 
